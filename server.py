3807b31700a434c31af95b0af4219dbda8313067
0D0AResearch <0A0DResearch@gmail.com>
2019-07-26 20:09:08 +1000

    Still trying to bind to the correct port. I'm not sure if I need to change it in the Python or the Heroku Procfile

--- server.py
+++ server.py
@@ -11,4 +11,5 @@ async def homepage(request):
     return JSONResponse({'hello':'world'})
 
 if __name__ == '_main__':
-    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT')))
+    port1 = int(os.environ.get('PORT', 5000))
+    uvicorn.run(app, host='0.0.0.0', port=port1)
